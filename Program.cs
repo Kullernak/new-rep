a6d301943565fa22deba8fb5917caf33fff2465e
Kullernak <mozgol33@gmail.com>
2024-01-03 20:26:33 +0300

    1 com

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int[] arr = new int[7];
+
+        Console.Write("пожалуйста введите 7 чисел: ");
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        int min = arr[0];
+        int max = arr[0];
+
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] < min)
+            {
+                min = arr[i];
+            }
+
+            if (arr[i] > max)
+            {
+                max = arr[i];
+            }
+        }
+
+        Console.WriteLine("минимальное значение: " + min);
+        Console.WriteLine("максимальное значение: " + max);
+    }
+}
