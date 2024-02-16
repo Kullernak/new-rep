6c11f41a3275a4e4f0a32adfa356ae6d25788013
Kullernak <mozgol33@gmail.com>
2024-02-16 23:02:32 +0300

    2

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+// Задаем массив строк вручную
+string[] words = { "hello", "world", "this", "is", "a", "test" };
+
+// Создаем новый список строк, куда будем добавлять строки длиной <= 3 символов
+List<string> shortWords = new List<string>();
+
+// Проходимся по каждой строке в исходном массиве
+foreach (string word in words)
+{
+    // Проверяем длину строки
+    if (word.Length <= 3)
+    {
+        // Если длина строки меньше или равна 3, добавляем ее в список
+        shortWords.Add(word);
+    }
+}
+
+// Преобразуем список обратно в массив
+string[] result = shortWords.ToArray();
+
+// Выводим результат на экран
+foreach (string word in result)
+{
+    Console.WriteLine(word);
+}
